4d652fcf3f132cc29195bff2bf1d0179e5c5f4e0
Yasemin Bridges <y.bridges@qmul.ac.uk>
2025-01-20 17:34:34 +0000

    make phenogenius work with poetry

--- src/tools/phenogenius.mk
+++ src/tools/phenogenius.mk
@@ -12,14 +12,13 @@ install-phenogenius:
 	cp src/tools/configs/phenogenius-config.yaml $(PHENOGENIUS_DIR)/2024-07-15/config.yaml
 	@echo "Installed $(PHENOGENIUS_NAME) in $(PHENOGENIUS_DIR)"
 
-# TODO make poetry work here
 # Run PhenoGenius
 run-phenogenius: venv
 	$(PIP) install poetry
 	cd $(PHENOGENIUS_DIR)/2024-07-15/pheval.phenogenius && $(VENV_NAME)/bin/poetry install
-	cd $(PHENOGENIUS_DIR)/2024-07-15/PhenoGeniusCli && $(VENV_NAME)/bin/poetry install
-	mkdir $(RESULTS_DIR)/$(PHENOGENIUS_NAME)-2024-07-15/phenopacket_store_0.1.11_phenotypes
-	$(VENV_NAME)/bin/pheval run --input-dir "$(PHENOGENIUS_DIR)/2024-07-15" \
+	mkdir -p $(RESULTS_DIR)/$(PHENOGENIUS_NAME)-2024-07-15/phenopacket_store_0.1.11_phenotypes
+	cd $(PHENOGENIUS_DIR)/2024-07-15/pheval.phenogenius && $(VENV_NAME)/bin/poetry run pheval run \
+		--input-dir "$(PHENOGENIUS_DIR)/2024-07-15" \
 		--testdata-dir "$(CORPORA_DIR)/phenopacket_store_0.1.11_phenotypes" \
 		--output-dir "$(RESULTS_DIR)/$(PHENOGENIUS_NAME)-2024-07-15/phenopacket_store_0.1.11_phenotypes" \
 		--runner phenogeniusphevalrunner --version $(PHENOGENIUS_VERSION)
